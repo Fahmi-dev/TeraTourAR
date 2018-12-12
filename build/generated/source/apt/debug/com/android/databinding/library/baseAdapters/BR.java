b4dfaf0a740d6f47abb24ff5c4fd78ef2021eb32
Francis Anyeji <anyeji.francis@gmail.com>
2018-12-12 12:06:15 +0100

    First Upload

---
+++ build/generated/source/apt/debug/com/android/databinding/library/baseAdapters/BR.java
@@ -0,0 +1,9 @@
+package com.android.databinding.library.baseAdapters;
+
+public class BR {
+  public static final int _all = 0;
+
+  public static final int targetInfo = 1;
+
+  public static final int arViewModel = 2;
+}
---
+++ build/generated/source/apt/debug/com/itera/teratour/BR.java
@@ -0,0 +1,9 @@
+package com.itera.teratour;
+
+public class BR {
+  public static final int _all = 0;
+
+  public static final int targetInfo = 1;
+
+  public static final int arViewModel = 2;
+}
